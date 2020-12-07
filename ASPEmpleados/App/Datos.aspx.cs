a502596bd3005fbc09cbae6a329f351a603a00dc
Ricardo Antonio Ruiz Cardozo <rauizcz@gmail.com>
2020-12-07 10:44:20 -0500

    first commit

---
+++ ASPEmpleados/App/Datos.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmpleadosCrud.App
+{
+    public partial class Datos : System.Web.UI.Page
+    {
+        // Crear nuestro objeto para Gestionar Datos
+        GestionDatos datos = new GestionDatos();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                List<Empleado> listaEmpleados = datos.LeerTodos();
+                // Volcamos la lista a nuestro gv
+                gvEmpleados.DataSource = listaEmpleados;
+                gvEmpleados.DataBind();
+            }
+        }
+
+        protected void btnListarTodo_Click(object sender, EventArgs e)
+        {
+            //En un a lista invocamos el objeto para listar los empleados
+            List<Empleado> listaEmpleados = datos.LeerTodos();
+            // Volcamos la lista a nuestro gv
+            gvEmpleados.DataSource = listaEmpleados;
+            gvEmpleados.DataBind();
+        }
+
+        protected void gvEmpleados_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvEmpleados.PageIndex = e.NewPageIndex;
+            List<Empleado> listaEmpleados = datos.LeerTodos();
+           
+            gvEmpleados.DataSource = listaEmpleados;
+            gvEmpleados.DataBind();
+        }
+
+        protected void btnBuscarCodigo_Click(object sender, EventArgs e)
+        {
+            if (datos.ExisteEmpleado(txtBuscarCodigo.Text))
+            {
+                Response.Redirect("Formulario.aspx?id=" + txtBuscarCodigo.Text);
+            }
+            else
+            {
+                LabelBuscar.Text = "No existe el código en la BD";
+            }
+        }
+
+        protected void gvEmpleados_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if(e.CommandName == "Select")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                string valor = Convert.ToString(gvEmpleados.DataKeys[index].Value);
+                Response.Redirect("Formulario.aspx?id=" + valor);
+            }
+        }
+    }
+}
---
+++ ASPEmpleados/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ASPEmpleados")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ASPEmpleados")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6cdef4c6-e635-42ef-bf2a-b476ab1c4b66")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
